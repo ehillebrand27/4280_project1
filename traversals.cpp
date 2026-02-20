d67cb19a50904f12442b76a3e7395587852c4616
Ethan Hillebrand <ethannhillebrand29@gmail.com>
2026-02-20 16:16:41 -0600

    added preorder and postorder functionality

--- traversals.cpp
+++ traversals.cpp
@@ -3,18 +3,97 @@
 #include "node.h"
 #include "traversals.h"
 #include <iostream>
+#include <cstring>
+#include <cstdio>
 using namespace std;
 
-static void traversePreOder(node_t *rootP, int level)
+
+void preTraversalHelper(node_t* rootP, FILE* out, int level)
 {
-    if (rootP == NULL) return;
+    if (rootP == nullptr) return;
 
-    // Visit Node
-    cout << node->data << " ";
+    // indentation
+    fprintf(out, "%*c", level * 4, ' ');
 
-    // Traverse left subtree
-    printPreOrder(node->left);
+    // level and ascii
+    fprintf(out, "%d %d ", level, rootP->ascii_number);
 
-    // Traverse right subtree
-    printPreOrder(node->right);
+    // print linked list
+    string_list* temp = rootP->list;
+    while (temp != nullptr)
+    {
+        fprintf(out, "%s ", temp->str.c_str());
+        temp = temp->next;
+    }
+
+    fprintf(out, "\n");
+
+    // recursive traversal
+    preTraversalHelper(rootP->left, out, level + 1);
+    preTraversalHelper(rootP->right, out, level + 1);
+
+}
+
+
+void traversePreOrder(node_t* root, const char baseFilename[])
+{
+    char filename[256];
+    sprintf(filename, "%s.preorder", baseFilename);
+
+    FILE* out = fopen(filename, "w");
+    if (!out)
+    {
+        fprintf(stderr, "Error opening preorder file\n");
+        return;
+    }
+
+    preTraversalHelper(root, out, 0);
+
+    fclose(out);
+}
+
+
+
+void postTraversalHelper(node_t* rootP, FILE* out, int level)
+{
+    if (rootP == nullptr) return;
+
+    // indentation
+    fprintf(out, "%*c", level * 4, ' ');
+
+    // level and ascii
+    fprintf(out, "%d %d ", level, rootP->ascii_number);
+
+    // print linked list
+    string_list* temp = rootP->list;
+    while (temp != nullptr)
+    {
+        fprintf(out, "%s ", temp->str.c_str());
+        temp = temp->next;
+    }
+
+    fprintf(out, "\n");
+
+    // recursive traversal
+    postTraversalHelper(rootP->right, out, level + 1);
+    postTraversalHelper(rootP->left, out, level + 1);
+
+}
+
+
+void traversePostOrder(node_t* root, const char baseFilename[])
+{
+    char filename[256];
+    sprintf(filename, "%s.postorder", baseFilename);
+
+    FILE* out = fopen(filename, "w");
+    if (!out)
+    {
+        fprintf(stderr, "Error opening preorder file\n");
+        return;
+    }
+
+    postTraversalHelper(root, out, 0);
+
+    fclose(out);
 }
--- traversals.h
+++ traversals.h
@@ -1,15 +1,15 @@
 
-#ifndef TRAVERSALS.H
-#define TRAVERSALS.H
-
-
-
+#ifndef TRAVERSALS_H
+#define TRAVERSALS_H
 #include "node.h"
 
 
 void traversePreOrder(node_t* root, const char baseFilename[]);
+void preTraversalHelper(node_t* rootP, int level);
 void traversePostOrder(node_t* root, const char baseFilename[]);
+void postTraversalHelper(node_t* rootP, int level);
 void traverseLevelOrder(node_t* root, const char baseFilename[]);
+void levelTraversalHelper(node_t* rootP, int level);
 
 #endif
 
